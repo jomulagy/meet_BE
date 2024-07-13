36280ff4642d7c8b1a74e379ec6facdfc82d3c30
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-07-13 17:35:04 +0900

    fix : mapper annotation 누락

--- src/main/java/com/example/meet/mapper/PlaceVoteItemMapper.java
+++ src/main/java/com/example/meet/mapper/PlaceVoteItemMapper.java
@@ -3,8 +3,10 @@ package com.example.meet.mapper;
 import com.example.meet.common.dto.request.place.CreatePlaceVoteItemRequestDto;
 import com.example.meet.entity.PlaceVote;
 import com.example.meet.entity.PlaceVoteItem;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface PlaceVoteItemMapper {
 
     PlaceVoteItemMapper INSTANCE = Mappers.getMapper(PlaceVoteItemMapper.class);
