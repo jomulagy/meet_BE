03ffbf0eecad2c6a28e3c5bf299924ed6451b3bd
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-09-04 23:08:22 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/meet/common/config/SecurityConfig.java
+++ src/main/java/com/example/meet/common/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("https://meetplace.store/"));
+                        config.setAllowedOrigins(Collections.singletonList("https://localhost:5173/"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
