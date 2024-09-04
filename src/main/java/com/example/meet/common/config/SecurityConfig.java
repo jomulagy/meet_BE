c7ad2b5ea4dbece24c9d9c58d6f40d5064e638e3
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-09-04 23:12:20 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/meet/common/config/SecurityConfig.java
+++ src/main/java/com/example/meet/common/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("https://localhost:5173/"));
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173/"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
