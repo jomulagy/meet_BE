e29b65c65243cec4e72440dd60c9704aaed3c984
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-10-29 01:03:22 +0900

    feat : create place entity

---
+++ src/main/java/com/example/meet/entity/Place.java
@@ -0,0 +1,38 @@
+package com.example.meet.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Table(name = "place")
+@Data
+public class Place {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "x_pos", precision = 20, scale = 14)
+    private BigDecimal xPos;
+
+    @Column(name = "y_pos", precision = 20, scale = 14)
+    private BigDecimal yPos;
+
+    @Column(name = "url")
+    private String url;
+
+    @OneToOne
+    @JoinColumn(name = "meet_id", referencedColumnName = "id")
+    private Meet meet;
+}
