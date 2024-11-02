6672ae75c12b0e88cf5f1fe4b9c284c551d6fea6
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-11-02 22:47:23 +0900

    fix : 잘못된 커밋 삭제

--- src/main/java/com/example/meet/common/dto/response/place/FindSimplePlaceResponseDto.java
+++ src/main/java/com/example/meet/common/dto/response/place/FindSimplePlaceResponseDto.java
@@ -12,13 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class FindSimplePlaceResponseDto {
     @Schema(description = "모임 장소", example = "강남역")
-    private String name;
-
-    @Schema(description = "x 좌표", example = "12.11")
-    private String xpos;
-
-    @Schema(description = "y 좌표", example = "123.45")
-    private String ypos;
+    private String value;
 
     @Schema(description = "수정 가능 여부", example = "true")
     private String editable;
