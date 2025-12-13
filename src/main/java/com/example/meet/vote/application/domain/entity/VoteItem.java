9d3b8e92a2baacd8f0610356674764e1360fb0df
김지훈 <87347699+jomulagy@users.noreply.github.com>
2025-12-13 16:22:29 +0900

    feat : vote 엔티티 통합

--- src/main/java/com/example/meet/vote/application/domain/entity/VoteItem.java
+++ src/main/java/com/example/meet/vote/application/domain/entity/VoteItem.java
@@ -1,24 +1,14 @@
 package com.example.meet.vote.application.domain.entity;
 
 import com.example.meet.entity.Member;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "vote_item")
@@ -32,14 +22,8 @@ public class VoteItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "is_participate")
-    private Boolean isParticipate;
-
-    @Column(name = "place")
-    private String place;
-
-    @Column(name = "date")
-    private LocalDateTime date;
+    @Column(name = "content")
+    private String content;
 
     @Column(name = "editable", nullable = false)
     private Boolean editable;
